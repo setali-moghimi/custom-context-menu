0b00ca1b73e4ae8d3a3f878f507bda124aeddef5
unknown <setalim7@gmail.com>
2023-08-12 15:08:54 +0330

    upload file of rtl click on github

---
+++ js/script.js
@@ -0,0 +1,18 @@
+﻿let meno= document.querySelector('#contextMenu')
+let bodi =document.body
+bodi.addEventListener('contextmenu',function(event){
+    event.preventDefault()
+    meno.style.display='flex'
+    meno.style.position='absolute'
+    meno.style.left=event.pageX
+    meno.style.top=event.pageY
+})
+bodi.addEventListener('click',function(event){
+    meno.style.display='none'
+})
+function space(event){
+    if(event.code=='Space'){
+        meno.style.display='none'
+
+    }
+}
